9cd4d1b157b16e9d21127346ff8c1a0d6ecb4a0f
spaceb0m <77018689+spaceb0m@users.noreply.github.com>
2026-07-01 14:10:37 +0200

    categories-noindex: plugin inicial
    Añade <meta robots noindex> en topics y páginas de listado de las
    categorías seleccionadas desde la administración. Las subcategorías
    se incluyen automáticamente.

---
+++ plugin.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# name: categories-noindex
+# about: Añade <meta robots noindex> a los topics y páginas de las categorías seleccionadas
+# version: 0.1
+# authors: Aldea Pucela
+# url: https://github.com/aldeapucela/discourse-categories-noindex
+
+enabled_site_setting :categories_noindex_enabled
+
+after_initialize do
+  builder = lambda do |controller|
+    category =
+      case controller
+      when TopicsController then controller.instance_variable_get(:@topic_view)&.topic&.category
+      when ListController   then controller.instance_variable_get(:@category)
+      end
+
+    selected = SiteSetting.categories_noindex_category_ids_map
+
+    # ponytail: sube por la cadena de ancestros para cubrir subcategorías a
+    # cualquier nivel. Al salir, `category` != nil sólo si hubo coincidencia.
+    while category
+      break if selected.include?(category.id)
+      category = category.parent_category
+    end
+
+    category ? '<meta name="robots" content="noindex">' : ""
+  end
+
+  register_html_builder("server:before-head-close-crawler", &builder)
+  register_html_builder("server:before-head-close", &builder)
+end
---
+++ spec/requests/categories_noindex_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+RSpec.describe "categories-noindex" do
+  fab!(:parent_cat) { Fabricate(:category) }
+  fab!(:sub_cat) { Fabricate(:category, parent_category: parent_cat) }
+  fab!(:other_cat) { Fabricate(:category) }
+  fab!(:noindex_post) { Fabricate(:post, topic: Fabricate(:topic, category: parent_cat)) }
+  fab!(:sub_post) { Fabricate(:post, topic: Fabricate(:topic, category: sub_cat)) }
+  fab!(:other_post) { Fabricate(:post, topic: Fabricate(:topic, category: other_cat)) }
+
+  let(:tag) { '<meta name="robots" content="noindex">' }
+
+  before do
+    SiteSetting.categories_noindex_enabled = true
+    SiteSetting.categories_noindex_category_ids = parent_cat.id.to_s
+  end
+
+  def crawl(path)
+    get path, headers: { "HTTP_USER_AGENT" => "Googlebot/2.1 (+http://www.google.com/bot.html)" }
+  end
+
+  it "marca los topics de la categoría seleccionada" do
+    crawl(noindex_post.topic.relative_url)
+    expect(response.body).to include(tag)
+  end
+
+  it "hereda a los topics de una subcategoría" do
+    crawl(sub_post.topic.relative_url)
+    expect(response.body).to include(tag)
+  end
+
+  it "marca la página de listado de la categoría" do
+    crawl("/c/#{parent_cat.slug}/#{parent_cat.id}")
+    expect(response.body).to include(tag)
+  end
+
+  it "NO marca los topics de otras categorías" do
+    crawl(other_post.topic.relative_url)
+    expect(response.body).not_to include('content="noindex"')
+  end
+end
